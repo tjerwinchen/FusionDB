8c9afc5bfe0a891b4b601c4d93830ba99dd87d8e
Theo Chen <tjerwinchen@gmail.com>
2014-07-14 14:28:50 +0800

    Initially Commit

---
+++ src/FusionDB.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : FusionDB.cpp
+// Author      : Theo Chen
+// Version     :
+// Copyright   : 2013 Theo
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
